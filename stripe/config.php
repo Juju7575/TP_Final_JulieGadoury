9ae32d596f429e69ff2c4bff0669ded4c1abbec1
Juju7575 <gadouryjulie@gmail.com>
2015-04-15 22:19:22 -0400

    Motif et ajout
    Ajout des liens au menu.
    Formater CSS page accueil. À finaliser
    Ajout code Google analytics.
    Ajout SDK Facebook.
    Ajout form pour Strype dans page magasin.html
    Reste a tester Strype sur serveur.

---
+++ stripe/config.php
@@ -0,0 +1,10 @@
+<?php
+require_once('stripe/init.php');
+
+$stripe = array(
+  "secret_key"      => "sk_test_VZV7Wd1JY3kqBkNG71NQs3kM",
+  "publishable_key" => "pk_test_2uVngJUlwknHJP2fLNyifFc0"
+);
+
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
+?>
